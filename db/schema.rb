ecb1e982b72b018936c9ec54ae7053e1dbdb71cb
Britt <saunders@actblue.com>
2017-06-06 20:13:19 -0400

    fixing databases for pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170605181521) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "b_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,15 +41,10 @@ ActiveRecord::Schema.define(version: 20170605181521) do
   end
 
   create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "phone"
+    t.text "name"
+    t.text "address"
+    t.bigint "phone"
     t.text "food_genre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
